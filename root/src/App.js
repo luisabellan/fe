7c5bb51a1c365358739083aa36643cc544e42e45
Tashby <tashby2714@gmail.com>
2020-05-24 16:12:10 -0400

    routes set up and homepage displayed

--- root/src/App.js
+++ root/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form.js';
+import Home from './components/Home.js';
+import {Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-     <Form />
+      <Route exact path="/" component={Home}/>
+      <Route path ="/form" component={Form} />
     </div>
   );
 }
